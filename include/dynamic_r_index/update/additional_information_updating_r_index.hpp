490bee17acadb550fdca2d86e02ea39b37afa2ad
Nishimoto <a32b16c4@gmail.com>
2025-06-04 15:25:57 +0900

    comment

--- include/dynamic_r_index/update/additional_information_updating_r_index.hpp
+++ include/dynamic_r_index/update/additional_information_updating_r_index.hpp
@@ -5,19 +5,30 @@ namespace stool
 {
     namespace dynamic_r_index
     {
+        /**
+         * @brief A structure that stores additional information needed during R-Index updates
+         * 
+         * This structure maintains various suffix array values and indices that are required
+         * during the dynamic update process of an R-Index data structure.
+         * It keeps track of positions y and z in the suffix array, along with values
+         * at those positions and their adjacent positions (y-1, y+1, z-1, z+1).
+         */
         struct AdditionalInformationUpdatingRIndex
         {
         public:
-            SAIndex y;
-            //int64_t y_star;
-            SAValue value_at_y;
-            SAValue value_at_y_star;
-            SAValue value_at_y_minus;
-            SAValue value_at_y_plus;
-            SAValue value_at_z_minus;
-            SAValue value_at_z_plus;
-            SAIndex z;
+            SAIndex y;              ///< Position y in the suffix array
+            //int64_t y_star;       
+            SAValue value_at_y;     ///< Value stored at position y in suffix array
+            SAValue value_at_y_star;///< Value stored at position y* in suffix array  
+            SAValue value_at_y_minus;///< Value stored at position y-1
+            SAValue value_at_y_plus;///< Value stored at position y+1
+            SAValue value_at_z_minus;///< Value stored at position z-1
+            SAValue value_at_z_plus;///< Value stored at position z+1
+            SAIndex z;              ///< Position z in the suffix array
 
+            /**
+             * @brief Default constructor that initializes all members to UINT64_MAX
+             */
             AdditionalInformationUpdatingRIndex()
             {
                 this->y = UINT64_MAX;
@@ -30,14 +41,16 @@ namespace stool
                 this->value_at_z_plus = UINT64_MAX;
                 this->z = UINT64_MAX;
             }
+
+            /**
+             * @brief Prints the current state of the structure to standard output
+             */
             void print() const
             {
                 std::cout << "AdditionalInformationUpdatingRIndex[ y = " << y  << ", SA[y] = " << value_at_y   << ", z = " << z << std::endl;
                 std::cout << "SA[y-1] = " << value_at_y_minus << ", SA[y+1] = " << value_at_y_plus << std::endl;
                 std::cout << "SA[z-1] = " << value_at_z_minus << ", SA[z+1] = " << value_at_z_plus << " ]" << std::endl;
-
             }
-            
         };
     }
 }
