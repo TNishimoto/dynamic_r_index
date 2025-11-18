0dd8c2cbf818b56cc08467b7c78608e5842f2c3e
Nishimoto <a32b16c4@gmail.com>
2025-11-18 16:04:50 +0900

    all

---
+++ include/all.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+/// \defgroup StringIndexes Main String Indexes Classes
+/// @{
+/// @}
+
+/// \defgroup DynamicRIndexes Classes for dynamic r-index
+/// @{
+/// @}
+
+/// \defgroup DynamicFMIndexes Classes for dynamic FM-index
+/// @{
+/// @}
+
+/// \defgroup MainDataStructures Main dynamic data structures
+/// @{
+/// @}
+
+
+#include "./dynamic_fm_index.hpp"
+#include "./dynamic_r_index.hpp"
+#include "./commands/query.hpp"
+// #include "./dynamic_fm_index/dynranks_wrapper.hpp"
